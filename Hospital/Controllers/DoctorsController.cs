08f05d58dd25e1cdb640a72659379396df272f3b
Abdelrahmanmoussan <abdelrahmanmoussan339@gmail.com>
2025-02-17 00:29:06 +0200

    Hospital Project

---
+++ Hospital/Controllers/DoctorsController.cs
@@ -0,0 +1,55 @@
+﻿using ContactDoctor.Models;
+using Hospital.DataAccess;
+using Hospital.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital.Controllers
+{
+    public class DoctorsController : Controller
+    {
+        ApplecationDbContext dbContext = new ApplecationDbContext();
+        public IActionResult BookAppointment()
+        {
+            var doctors = dbContext.Doctors;
+            return View(doctors.ToList());
+        }
+
+
+        public IActionResult CompleteAppointment(string doctorName)
+        {
+            var doctor = dbContext.Doctors.FirstOrDefault(d => d.Name == doctorName);
+
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            return View(doctor);
+        }
+
+
+        [HttpGet]
+        public IActionResult Index()
+        { 
+            var appointments = dbContext.PatientAppointments;
+            return View(appointments.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult Index(PatientAppointment patientAppointment)
+        {
+            dbContext.PatientAppointments.Add(new PatientAppointment
+            {
+                PatientName = patientAppointment.PatientName,
+                AppointmentDate = patientAppointment.AppointmentDate,
+                AppointmentTime = patientAppointment.AppointmentTime,
+                DoctorId = patientAppointment.DoctorId,
+                DoctorName = patientAppointment.DoctorName
+            });
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
